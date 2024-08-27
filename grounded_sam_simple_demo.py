cccfcceced0118890768fe03b56574b5736ebf22
Robin.Wang <3Jg$94sbQ@q1>
2024-08-27 15:02:05 +0800

    feat(simple_demo): running success

--- grounded_sam_simple_demo.py
+++ grounded_sam_simple_demo.py
@@ -5,10 +5,11 @@ import supervision as sv
 import torch
 import torchvision
 
-from groundingdino.util.inference import Model
+from GroundingDINO.groundingdino.util.inference import Model
 from segment_anything import sam_model_registry, SamPredictor
 
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 
 # GroundingDINO config and checkpoint
 GROUNDING_DINO_CONFIG_PATH = "GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
@@ -19,13 +20,13 @@ SAM_ENCODER_VERSION = "vit_h"
 SAM_CHECKPOINT_PATH = "./sam_vit_h_4b8939.pth"
 
 # Building GroundingDINO inference model
-grounding_dino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH)
+grounding_dino_model = Model(model_config_path=GROUNDING_DINO_CONFIG_PATH,
+                             model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH, device='mps')
 
 # Building SAM Model and SAM Predictor
 sam = sam_model_registry[SAM_ENCODER_VERSION](checkpoint=SAM_CHECKPOINT_PATH)
 sam.to(device=DEVICE)
 sam_predictor = SamPredictor(sam)
-
 
 # Predict classes and hyper-param for GroundingDINO
 SOURCE_IMAGE_PATH = "./assets/demo2.jpg"
@@ -33,7 +34,6 @@ CLASSES = ["The running dog"]
 BOX_THRESHOLD = 0.25
 TEXT_THRESHOLD = 0.25
 NMS_THRESHOLD = 0.8
-
 
 # load image
 image = cv2.imread(SOURCE_IMAGE_PATH)
@@ -49,20 +49,20 @@ detections = grounding_dino_model.predict_with_classes(
 # annotate image with detections
 box_annotator = sv.BoxAnnotator()
 labels = [
-    f"{CLASSES[class_id]} {confidence:0.2f}" 
-    for _, _, confidence, class_id, _, _ 
+    f"{CLASSES[class_id]} {confidence:0.2f}"
+    for _, _, confidence, class_id, _, _
     in detections]
-annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
+# annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections, labels=labels)
+annotated_frame = box_annotator.annotate(scene=image.copy(), detections=detections)
 
 # save the annotated grounding dino image
-cv2.imwrite("groundingdino_annotated_image.jpg", annotated_frame)
-
+cv2.imwrite("./dist/groundingdino_annotated_image.jpg", annotated_frame)
 
 # NMS post process
 print(f"Before NMS: {len(detections.xyxy)} boxes")
 nms_idx = torchvision.ops.nms(
-    torch.from_numpy(detections.xyxy), 
-    torch.from_numpy(detections.confidence), 
+    torch.from_numpy(detections.xyxy),
+    torch.from_numpy(detections.confidence),
     NMS_THRESHOLD
 ).numpy().tolist()
 
@@ -71,6 +71,7 @@ detections.confidence = detections.confidence[nms_idx]
 detections.class_id = detections.class_id[nms_idx]
 
 print(f"After NMS: {len(detections.xyxy)} boxes")
+
 
 # Prompting SAM with detected boxes
 def segment(sam_predictor: SamPredictor, image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
@@ -97,11 +98,12 @@ detections.mask = segment(
 box_annotator = sv.BoxAnnotator()
 mask_annotator = sv.MaskAnnotator()
 labels = [
-    f"{CLASSES[class_id]} {confidence:0.2f}" 
-    for _, _, confidence, class_id, _, _ 
+    f"{CLASSES[class_id]} {confidence:0.2f}"
+    for _, _, confidence, class_id, _, _
     in detections]
 annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
-annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+# annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections)
 
 # save the annotated grounded-sam image
-cv2.imwrite("grounded_sam_annotated_image.jpg", annotated_image)
+cv2.imwrite("./dist/grounded_sam_annotated_image.jpg", annotated_image)
